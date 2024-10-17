83849d92b480db35ac788cdf5e4697b85bda63ad
BigFigWasp <sns@posteo.de>
2024-10-17 14:06:23 +0200

    PayPal Donate button

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://jan.homes',
   integrations: [tailwind()],
   base: '/saturn/',
 });
